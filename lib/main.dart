fd7fde3c8ca901cd58b3c88ebdb92aa5e7dd4d7a
Abdul Ragib <70109434+abdulragib@users.noreply.github.com>
2022-02-21 20:42:02 +0530

    adding const

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:package/package.dart';
 
-void main() =>runApp(MaterialApp(home: packageDemo()));
-
+void main() => runApp(const MaterialApp (
+    debugShowCheckedModeBanner: false,
+    home: packageDemo()));
 
 class packageDemo extends StatelessWidget {
   const packageDemo({Key? key}) : super(key: key);
@@ -11,16 +12,15 @@ class packageDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            await CustomAlertBox.showCustomAlertBox(
-                context: context,
-                willDisplayWidget: Container(
-                  child: Text('My custom alert box, used from example!!'),
-                ));
-          },
-          tooltip: 'Show Custom Alert Box',
-          child: Icon(Icons.message),
-        )
-    );
+      onPressed: () async {
+        await CustomAlertBox.showCustomAlertBox(
+            context: context,
+            willDisplayWidget: Container(
+              child: Text('My custom alert box, used from example!!'),
+            ));
+      },
+      tooltip: 'Show Custom Alert Box',
+      child: Icon(Icons.message),
+    ));
   }
 }
