ff4d1799f738546facf948c097669027c49fa2ab
Abdul Ragib <70109434+abdulragib@users.noreply.github.com>
2022-02-21 10:09:03 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/guru/demo01/demo01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.guru.demo01.demo01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:package/package.dart';
+
+void main() =>runApp(MaterialApp(home: packageDemo()));
+
+
+class packageDemo extends StatelessWidget {
+  const packageDemo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            await CustomAlertBox.showCustomAlertBox(
+                context: context,
+                willDisplayWidget: Container(
+                  child: Text('My custom alert box, used from example!!'),
+                ));
+          },
+          tooltip: 'Show Custom Alert Box',
+          child: Icon(Icons.message),
+        )
+    );
+  }
+}
